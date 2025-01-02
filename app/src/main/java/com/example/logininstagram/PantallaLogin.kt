570e45bded7bb72df2499cf689271f86bed8a019
Carles <carleslop05@gmail.com>
2025-01-02 12:44:21 +0100

    v2.0

--- app/src/main/java/com/example/logininstagram/PantallaLogin.kt
+++ app/src/main/java/com/example/logininstagram/PantallaLogin.kt
@@ -2,18 +2,29 @@ package com.example.logininstagram
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -29,11 +40,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
@@ -45,7 +58,41 @@ fun PantallaLogin() {
     )   {
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
-        //Footer(Modifier.align(Alignment.BottomCenter))
+        Footer(Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+@Composable
+fun Footer(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        HorizontalDivider(
+            Modifier
+                .background(Color(0xFF9F9F9F))
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Don't have an account?",
+                fontSize = 12.sp,
+                color = Color(0xFFB5B5B5),
+                modifier = Modifier.padding(horizontal = 8.dp)
+            )
+            Text(
+                text = "Sign Up",
+                fontSize = 12.sp,
+                color = Color(0xFF4EA8E9),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.clickable { createAccount() }
+            )
+        }
     }
 }
 
@@ -61,15 +108,117 @@ fun Header(modifier: Modifier) {
 fun Body(modifier: Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
-    Column( modifier = modifier) {
-        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
-        Email(email) { email = it }
-        Password(password) { password = it }
-//        ForgotPass()
-//        LoginButton()
-//        LoginDivisor()
-//        LoginSocial()
+    var isLoginEnable by rememberSaveable { mutableStateOf(false) }
+
+    fun updateLoginState() {
+        isLoginEnable = email.isNotBlank() && password.isNotBlank()
     }
+
+        Column( modifier = modifier) {
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email) {
+            email = it
+            updateLoginState()
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        Password(password) {
+            password = it
+            updateLoginState()
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPass(Modifier.align(Alignment.End))
+        Spacer(modifier = Modifier.size(16.dp))
+        LoginButton(isLoginEnable)
+        Spacer(modifier = Modifier.size(16.dp))
+        LoginDivisor()
+        Spacer(modifier = Modifier.size(32.dp))
+        LoginSocial()
+    }
+}
+
+@Composable
+fun LoginSocial() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fb),
+            contentDescription = "FB",
+            modifier = Modifier.size(16.dp)
+        )
+
+        Text(
+            text = "Continue as Carles",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9),
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .clickable { checkSocialLogin() }
+        )
+    }
+}
+
+@Composable
+fun LoginDivisor() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HorizontalDivider(
+            Modifier
+                .background(Color(0xFF9F9F9F))
+                .height(1.dp)
+                .weight(1f)
+        )
+
+        Text(
+            text = "OR",
+            modifier = Modifier.padding(horizontal = 6.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 12.sp,
+            color = Color(0xFFB5B5B5)
+        )
+
+        HorizontalDivider(
+            Modifier
+                .background(Color(0xFF9F9F9F))
+                .height(1.dp)
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean) {
+    Button(
+        onClick = { ckeckLogin() },
+        enabled = loginEnable,
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            disabledContentColor = Color.White,
+            containerColor = Color(0xFF4EA8E9),
+            disabledContainerColor = Color(0xFF78C8F9)
+        )
+    ) {
+        Text(text = "Log In")
+    }
+}
+
+@Composable
+fun ForgotPass(modifier: Modifier) {
+    Text(
+        text = "Forgot password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF4EA8E9),
+        modifier = modifier.
+        clickable { sendNewPassword() }
+    )
 }
 
 @Composable
@@ -124,7 +273,7 @@ fun Email(email: String, function: (String) -> Unit) {
             unfocusedIndicatorColor = Color.Transparent
         ),
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        placeholder = { Text(text = "Email") }
+        placeholder = { Text(text = "Email") },
     )
 }
 
@@ -135,4 +284,20 @@ fun ImageLogo(modifier: Modifier) {
         contentDescription = "Logo",
         modifier = modifier
     )
+}
+
+fun ckeckLogin() {
+    TODO("Not yet implemented")
+}
+
+fun sendNewPassword() {
+    TODO("Not yet implemented")
+}
+
+fun checkSocialLogin() {
+    TODO("Not yet implemented")
+}
+
+fun createAccount() {
+    TODO("Not yet implemented")
 }
